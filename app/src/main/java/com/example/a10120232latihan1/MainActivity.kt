e4ee3ecabd61182164da0a645f4a859967fbe5d0
TriRamdhanSeptiadi <todoraki240@gmail.com>
2023-04-12 14:13:03 +0700

    First Commit

---
+++ app/src/main/java/com/example/a10120232latihan1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.a10120232latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Tanggal Pengerjaan   : 10 April 2023
+// NIM                  : 10120232
+// Nama                 : Tri Ramdhan Septiadi
+// Kelas                : IF6
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/a10120232latihan1/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+package com.example.a10120232latihan1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+// Tanggal Pengerjaan   : 10 April 2023
+// NIM                  : 10120232
+// Nama                 : Tri Ramdhan Septiadi
+// Kelas                : IF6
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
